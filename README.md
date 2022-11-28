2e2d39b4c2e8dcc79613b0aa5c2c1c6000abed58
ianrich69420 <ian.orieko@student.moringaschool.com>
2022-11-28 12:03:34 +0300

    Completed assignment

---
+++ README.md
@@ -0,0 +1,33 @@
+# Phase 1 Week 1 Moringa Code Challenge
+
+## Tasks
+
+- Challenge 1: Student Grade Generator (Toy Problem)
+Write that prompts the user to input student marks. The input should be between 0 and 100. Then output the correct grade: 
+
+A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40.
+
+- Challenge 2: Speed Detector (Toy Problem)
+Write a program that takes as input the speed of a car e.g 80. If the speed is less than 70, it should print “Ok”. Otherwise, for every 5 km/s above the speed limit (70), it should give the driver one demerit point and print the total number of demerit points.
+
+For example, if the speed is 80, it should print: “Points: 2”. If the driver gets more than 12 points, the function should print: “License suspended”.
+
+- Challenge 3: Net Salary Calculator (Toy Problem)
+Write a program whose major task is to calculate an individual’s Net Salary by getting the inputs of basic salary and benefits. Calculate the payee (i.e. Tax), NHIFDeductions, NSSFDeductions, gross salary, and net salary. 
+NB: Use KRA, NHIF, and NSSF values provided in the link below.
+
+https://www.aren.co.ke/payroll/taxrates.htm 
+
+https://www.kra.go.ke/en/individual/calculate-tax/calculating-tax/paye
+
+## Project Setup
+
+- The project will be setup in JavaScript
+
+# Learn.co Educational Content License
+
+Copyright (c) 2017 Flatiron School, Inc.
+
+The Flatiron School, Inc. owns this Educational Content. However, the Flatiron School supports the development and availability of educational materials in the public domain. Therefore, the Flatiron School grants Users of the Flatiron Educational Content set forth in this repository certain rights to reuse, build upon and share such Educational Content subject to the terms of the Educational Content License set forth [here](http://learn.co/content-license) (http://learn.co/content-license). You must read carefully the terms and conditions contained in the Educational Content License as such terms govern access to and use of the Educational Content.
+
+Flatiron School is willing to allow you access to and use of the Educational Content only on the condition that you accept all of the terms and conditions contained in the Educational Content License set forth [here](http://learn.co/content-license) (http://learn.co/content-license). By accessing and/or using the Educational Content, you are agreeing to all of the terms and conditions contained in the Educational Content License. If you do not agree to any or all of the terms of the Educational Content License, you are prohibited from accessing, reviewing or using in any way the Educational Content. 
---
+++ index.js
@@ -0,0 +1,124 @@
+function studentGradeGenerator (marks) { //Defining the function the will generate the Student Grades
+    marks = prompt("Please enter the student marks correctly", 100) //Prompting the user to input the marks to be converted
+
+    if (marks > 79 && marks <= 100) { //if statement accounting for the requirements for a grade A
+        console.log("A")
+    }   else if (marks > 59 && marks <= 79) { //else if statement accounting for the requirements for a grade B
+            console.log("B")
+        }   else if (marks > 49 && marks <= 59) { //else if statement accounting for the requirements for a grade C
+                console.log("C")
+            }   else if (marks > 39 && marks <= 49) { //else if statement accounting for the requirements for a grade D
+                    console.log("D")
+                }   else if (marks >= 0 && marks <= 39) { //default failing grade accounting for the requirements for a grade E if all previous if's and else if's are not passed
+                        console.log("E")
+                    }   else {
+                            studentGradeGenerator(marks) //Accounting for misinputs
+                            }
+                    
+}
+
+function speedDetector (speed) { //Defining the function that will calculate the speeds
+    const speedLimit = 75 //Defining the speed limit
+    let demeritCount = 0 //Initializing a counter for the demerits
+    const maxDemerits = 12 //Defining the maximum number of demerits before license suspension
+    if (speed > 0 && speed < speedLimit) { //if statement accounting for speeds that are not eligible to receive a demerit
+        console.log("Ok")
+    }   else if (speed >= speedLimit) { //if statement accounting for the amount of demerits the speed is eligible for
+            demeritCount = (speed - 70) / 5
+            console.log(`Points: ${demeritCount}`)
+            if (demeritCount >= maxDemerits) {
+                console.log("License suspended")
+            }
+        }
+}
+
+function netSalaryCalculator (basicSalary, benefits) {
+    let tax;
+    let payee = tax * basicSalary;
+    let nhifDeduction;
+    let pensionablePay = 0.06 * basicSalary
+    let grossSalary = payee
+    let netSalary = payee - nhifDeduction - pensionablePay
+    if (basicSalary > 0 && basicSalary <= 24000) {
+        tax = 0.1
+        console.log(`Your tax is ${payee}`)
+    }   else if (basicSalary > 24000 && basicSalary <= 32333) {
+            tax = 0.25
+            console.log(`Your tax is ${payee}`)
+        } else if (basicSalary > 32333) {
+                tax = 0.3
+                console.log(`Your payee is ${payee}`)
+            }
+
+    if (basicSalary <= 5999) {
+        nhifDeduction = 150
+        console.log(`Your NHIF Deduction is ${nhifDeduction}`)   
+    }   else if (basicSalary > 5999 && basicSalary <= 7999) {
+            nhifDeduction = 300
+            console.log(`Your NHIF Deduction is ${nhifDeduction}`) 
+        }   else if (basicSalary > 7999 && basicSalary <= 11999) {
+                nhifDeduction = 400 
+                console.log(`Your NHIF Deduction is ${nhifDeduction}`)    
+            }   else if (basicSalary > 11999 && basicSalary <= 14999) {
+                    nhifDeduction = 500 
+                    console.log(`Your NHIF Deduction is ${nhifDeduction}`)
+                }   else if (basicSalary > 5999 && basicSalary <= 7999) {
+                        nhifDeduction = 300
+                        console.log(`Your NHIF Deduction is ${nhifDeduction}`) 
+                    }   else if (basicSalary > 7999 && basicSalary <= 11999) {
+                            nhifDeduction = 400 
+                            console.log(`Your NHIF Deduction is ${nhifDeduction}`)    
+                        }   else if (basicSalary > 11999 && basicSalary <= 14999) {
+                                nhifDeduction = 500 
+                                console.log(`Your NHIF Deduction is ${nhifDeduction}`)
+                            }   else if (basicSalary > 14999 && basicSalary <= 19999) {
+                                    nhifDeduction = 600
+                                    console.log(`Your NHIF Deduction is ${nhifDeduction}`) 
+                                }   else if (basicSalary > 19999 && basicSalary <= 24999) {
+                                        nhifDeduction = 750 
+                                        console.log(`Your NHIF Deduction is ${nhifDeduction}`)    
+                                    }   else if (basicSalary > 24999 && basicSalary <= 29999) {
+                                            nhifDeduction = 850
+                                            console.log(`Your NHIF Deduction is ${nhifDeduction}`)
+                                        }   else if (basicSalary > 29999 && basicSalary <= 34999) {
+                                                nhifDeduction = 900
+                                                console.log(`Your NHIF Deduction is ${nhifDeduction}`) 
+                                            }   else if (basicSalary > 34999 && basicSalary <= 39999) {
+                                                    nhifDeduction = 950 
+                                                    console.log(`Your NHIF Deduction is ${nhifDeduction}`)    
+                                                }   else if (basicSalary > 39999 && basicSalary <= 44999) {
+                                                        nhifDeduction = 1000 
+                                                        console.log(`Your NHIF Deduction is ${nhifDeduction}`)
+                                                    }   else if (basicSalary > 44999 && basicSalary <= 49999) {
+                                                            nhifDeduction = 1100 
+                                                            console.log(`Your NHIF Deduction is ${nhifDeduction}`)
+                                                        }   else if (basicSalary > 49999 && basicSalary <= 59999) {
+                                                                nhifDeduction = 1200
+                                                                console.log(`Your NHIF Deduction is ${nhifDeduction}`) 
+                                                            }   else if (basicSalary > 59999 && basicSalary <= 69999) {
+                                                                    nhifDeduction = 1300 
+                                                                    console.log(`Your NHIF Deduction is ${nhifDeduction}`)    
+                                                                }   else if (basicSalary > 69999 && basicSalary <= 79999) {
+                                                                        nhifDeduction = 1400
+                                                                        console.log(`Your NHIF Deduction is ${nhifDeduction}`)
+                                                                    }   else if (basicSalary > 79999 && basicSalary <= 89999) {
+                                                                            nhifDeduction = 1500
+                                                                            console.log(`Your NHIF Deduction is ${nhifDeduction}`) 
+                                                                        }   else if (basicSalary > 89999 && basicSalary <= 99999) {
+                                                                                nhifDeduction = 1600 
+                                                                                console.log(`Your NHIF Deduction is ${nhifDeduction}`)    
+                                                                            }   else if (basicSalary > 99999) {
+                                                                                    nhifDeduction = 1700 
+                                                                                    console.log(`Your NHIF Deduction is ${nhifDeduction}`)
+                                                                                }   
+    if (pensionablePay > 0 && pensionablePay < 6000) {
+        console.log(`Your Pensionable Pay is Tier 1 of amount ${pensionablePay}`)
+    }   else if (pensionablePay > 6000 && pensionablePay <= 18000) {
+            console.log(`Your Pensionable Pay is Tier 1 of amount ${pensionablePay}`)
+        }   else {
+                console.log(`Your Pensionable Pay is Tier 2 of amount 18000`)
+            }
+
+    console.log(grossSalary)
+    console.log(netSalary)
+}
